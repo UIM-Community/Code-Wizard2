54c8e9bfd842b8949bac14b3b88fce93826b1602
fraxken <gentilhomme.thomas@gmail.com>
2017-04-24 20:30:27 +0200

    Add font awesome

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({ 
     width: 950, 
     height: 700, 
-    frame: true
+    frame: false
   });
 
   mainWindow.loadURL(url.format({
@@ -19,7 +19,7 @@ function createWindow() {
     slashes: true
   }));
 
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     mainWindow = null;
